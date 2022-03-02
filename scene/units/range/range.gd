f9a435727ccdae934a283b2b845d143b7419ce26
renosyah <syahputrareno975@gmail.com>
2022-03-02 18:44:32 +0700

    remove rpc function shot at for range unit and mounted range unit

--- scene/units/range/range.gd
+++ scene/units/range/range.gd
@@ -1,27 +1,14 @@
 extends "res://scene/units/melee/melee.gd"
 
+onready var _shot_delay = $shot_delay
 var projectile_scene = ""
 
-############################################################
-# multiplayer func
-remotesync func _shot_at(target_translation : Vector3, from : Dictionary):
-	var arrow = load(projectile_scene).instance()
-	arrow.attack_damage = attack_damage
-	arrow.team = team
-	arrow.color = color
-	arrow.player = from
-	arrow.is_master = is_master()
-	arrow.spread = 0.4
-	add_child(arrow)
-	arrow.translation = translation
-	arrow.launch(target_translation)
-	
-	._perform_attack()
-
-############################################################
 func set_data(_data):
 	.set_data(_data)
 	projectile_scene = _data.projectile_scene
+	
+func _ready():
+	_shot_delay.wait_time = attack_cooldown
 	
 func perform_attack():
 	#.perform_attack()
@@ -34,5 +21,59 @@ func perform_attack():
 			capture_damage,
 			Utils.create_hit_by(player, self.get_path(), team, color)
 		)
+	
+func moving(delta):
+	.moving(delta)
+	
+	if is_dead:
+		return
 		
-	rpc("_shot_at", target.translation, player)
+	if not target:
+		return
+		
+	if not is_instance_valid(target):
+		return
+		
+	if not target.is_targetable(team):
+		return
+		
+	var distance_to_target = translation.distance_to(target.translation)
+	if distance_to_target <= range_attack + 5.0:
+		if _shot_delay.is_stopped():
+			_fire()
+			_shot_delay.start()
+		
+func _fire():
+	var arrow = load(projectile_scene).instance()
+	arrow.attack_damage = attack_damage
+	arrow.team = team
+	arrow.color = color
+	arrow.player = player
+	arrow.is_master = is_master()
+	arrow.spread = 0.4
+	add_child(arrow)
+	arrow.translation = translation
+	arrow.launch(target.translation)
+	
+	._perform_attack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
